fa45c623688741e5a0c5595ef362a303824923a0
Max <maximusekeh@gmail.com>
2026-05-07 12:05:38 +0000

    Cleanup genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,4 +1,4 @@
-import { HStack, Image, Link, List, Spinner } from "@chakra-ui/react";
+import { Heading, HStack, Image, Link, List, Spinner } from "@chakra-ui/react";
 import useGenres, { type Genre } from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
 
@@ -11,27 +11,32 @@ const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
   const { data, isLoading } = useGenres();
   if (isLoading) return <Spinner />;
   return (
-    <List.Root listStyleType="none">
-      {data.map((genre) => (
-        <List.Item key={genre.id} paddingY={1.5}>
-          <HStack>
-            <Image
-              boxSize="32px"
-              borderRadius="full"
-              src={getCroppedImageUrl(genre.image_background)}
-            />
-            <Link
-              fontSize="md"
-              fontWeight={genre.id == selectedGenre?.id ? "bolder" : "medium"}
-              variant="underline"
-              onClick={() => onSelectGenre(genre)}
-            >
-              {genre.name}
-            </Link>
-          </HStack>
-        </List.Item>
-      ))}
-    </List.Root>
+    <>
+      <Heading mb="3" size="xl">
+        Genres
+      </Heading>
+      <List.Root listStyleType="none">
+        {data.map((genre) => (
+          <List.Item key={genre.id} paddingY={1.5}>
+            <HStack>
+              <Image
+                boxSize="32px"
+                borderRadius="full"
+                src={getCroppedImageUrl(genre.image_background)}
+              />
+              <Link
+                fontSize="md"
+                fontWeight={genre.id == selectedGenre?.id ? "bolder" : "medium"}
+                variant="underline"
+                onClick={() => onSelectGenre(genre)}
+              >
+                {genre.name}
+              </Link>
+            </HStack>
+          </List.Item>
+        ))}
+      </List.Root>
+    </>
   );
 };
 
