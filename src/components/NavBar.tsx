4d14f03b250a9d3778ab67f1fc99c91d6bda0dd9
Max <maximusekeh@gmail.com>
2026-05-15 15:39:56 +0000

    Fix Nav bar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,11 +2,14 @@ import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
+import { Link } from "react-router";
 
 const NavBar = () => {
   return (
     <HStack padding="10px">
-      <Image src={logo} boxSize={"50px"}></Image>
+      <Link to="/">
+        <Image src={logo} boxSize={"50px"}></Image>
+      </Link>
       <SearchInput />
       <ColorModeSwitch />
     </HStack>
--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -2,16 +2,22 @@ import { Input, InputGroup } from "@chakra-ui/react";
 import { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
 import useGameQueryStore from "../store/gameQueryStore";
+import { useNavigate } from "react-router";
 
 const SearchInput = () => {
   const ref = useRef<HTMLInputElement>(null);
   const setSearchText = useGameQueryStore((s) => s.setSearchText);
+  const navigate = useNavigate();
 
   return (
     <form
       onSubmit={(event) => {
         event.preventDefault();
-        if (ref) setSearchText(ref.current?.value);
+
+        if (ref) {
+          setSearchText(ref.current?.value);
+          navigate("/");
+        }
       }}
     >
       <InputGroup startElement={<BsSearch />}>
